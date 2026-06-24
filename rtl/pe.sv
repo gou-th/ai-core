9e29b42becf957ae916d5bc927d210ac61e12107
gou-th <raghurgk03@gmail.com>
2026-06-25 00:51:15 +0530

    Added verified INT8 PE  along with cocotb testbench and its Makefile

---
+++ rtl/pe.sv
@@ -0,0 +1,36 @@
+module pe #(
+    parameter DATA_WIDTH = 8
+)
+(
+    input logic clk,
+    input logic rst_n,
+    input logic signed [DATA_WIDTH-1:0] act_in,
+    input logic signed [DATA_WIDTH-1:0] weight,
+    input logic signed [4*DATA_WIDTH-1:0] psum_in,
+    input logic load_w,
+    output logic signed [DATA_WIDTH-1:0] act_out,
+    output logic signed [4*DATA_WIDTH-1:0] psum_out
+);
+
+reg signed [DATA_WIDTH-1:0] weight_reg;
+
+always @(posedge clk) begin
+    if (!rst_n) begin
+        act_out <= 0;
+        psum_out <= 0;
+        weight_reg <= 0;
+    end else begin
+        if (load_w) 
+            weight_reg <= weight; // Load weight into register
+        // Perform MAC operation
+        act_out <= act_in; // Pass activation to next PE
+        psum_out <= psum_in + (act_in*weight_reg); // Update partial sum
+    end
+end
+
+initial begin
+    $dumpfile("pe.vcd");
+    $dumpvars(0, pe);
+end
+
+endmodule
