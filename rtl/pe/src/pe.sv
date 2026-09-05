faed5b000869d3c57219964b197d5f572dc20fff
gou-th <raghurgk03@gmail.com>
2026-09-05 22:25:06 +0530

    Modified pe.sv to make it use DSP slices in Vivado

--- rtl/pe/src/pe.sv
+++ rtl/pe/src/pe.sv
@@ -1,30 +1,31 @@
 module pe #(
     parameter DATA_WIDTH = 8
-)
-(
-    input logic clk,
-    input logic rst_n,
-    input logic signed [DATA_WIDTH-1:0] act_in,
-    input logic signed [DATA_WIDTH-1:0] weight,
-    input logic signed [4*DATA_WIDTH-1:0] psum_in,
+)(
+    input logic clk, rst_n,
     input logic load_w,
+    input logic signed [DATA_WIDTH-1:0] act_in, weight,
+    input logic signed [4*DATA_WIDTH-1:0] psum_in,
     output logic signed [DATA_WIDTH-1:0] act_out,
     output logic signed [4*DATA_WIDTH-1:0] psum_out
 );
 
 reg signed [DATA_WIDTH-1:0] weight_reg;
 
-always @(posedge clk or negedge rst_n) begin
+(* use_dsp = "yes" *)
+logic signed [2*DATA_WIDTH-1:0] product;
+
+assign product = act_in * weight_reg;
+
+always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         act_out <= 0;
         psum_out <= 0;
         weight_reg <= 0;
     end else begin
-        if (load_w) 
-            weight_reg <= weight; // Load weight into register
-        // Perform MAC operation
-        act_out <= act_in; // Pass activation to next PE
-        psum_out <= psum_in + (act_in*weight_reg); // Update partial sum
+        if (load_w)
+            weight_reg <= weight;
+        act_out <= act_in;
+        psum_out <= psum_in + {{(4*DATA_WIDTH-2*DATA_WIDTH){product[2*DATA_WIDTH-1]}}, product};
     end
 end
 
